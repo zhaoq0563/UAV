f0812d67f28804a80e72efee12da171c96585a17
Qi Zhao <zhaoq0563@gmail.com>
2018-02-01 10:05:27 +0800

    update the plot script

--- plot_throughptu.py
+++ plot_throughptu.py
@@ -21,31 +21,27 @@ while line:
 f.close()
 f=open("sptcp.txt",'r')
 f.readline()
-sp_x, sp_wlan, sp_sat=([]for i in range(3))
+sp_x, sp_sat, sp_wlan=([]for i in range(3))
 line=f.readline()
 while line:
     l=line.split(",")
     for k in range(len(l)):
         l[k]=l[k].strip("\n")
         l[k]=l[k].strip("\"")
-    x,wlan,sat=l
+    x,sat,wlan=l
     sp_x.append(float(x))
     sp_wlan.append(float(wlan)/10**6)
     sp_sat.append(float(sat)/10**6)
     line=f.readline()
 f.close()
-color_sequence = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
-                  '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
-                  '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
-                  '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
 fig, axes = plt.subplots(nrows=2,ncols=1, figsize=(20,4))
 ax1=axes[0]
 ax2=axes[1]
-ax1.plot(fdm_x,fdm_wlan,color=color_sequence[0],linestyle=':',marker='x',markersize=3,label='fdm_wlan')
-ax1.plot(fdm_x,fdm_sat,color=color_sequence[2],linestyle=':',marker='x',markersize=3,label='fdm_sat')
-ax1.plot(fdm_x,fdm_tot,color='black',linestyle='--',linewidth=1,label='fdm_tot')
-ax2.plot(sp_x,sp_wlan,color=color_sequence[0],linestyle=':',marker='x',markersize=3,label='sp_wlan')
-ax2.plot(sp_x,sp_sat,color=color_sequence[2],linestyle=':',marker='x',markersize=3,label='sp_sat')
+ax1.plot(fdm_x,fdm_wlan,color='#FE0000',linestyle=':',marker='x',markersize=3,label='mptcp_wlan')
+ax1.plot(fdm_x,fdm_sat,color='#27BAFF',linestyle=':',marker='o',markersize=3,label='mptcp_sat')
+ax1.plot(fdm_x,fdm_tot,color='#FFD800',linestyle='--',linewidth=3,label='mptcp_total')
+ax2.plot(sp_x,sp_wlan,color='#FE0000',linestyle=':',marker='x',markersize=3,label='sptcp_wlan')
+ax2.plot(sp_x,sp_sat,color='#27BAFF',linestyle=':',marker='o',markersize=3,label='sptcp_sat')
 ax1.set_xlim(0, 180)
 ax1.set_ylim(0, 2)
 ax2.set_xlim(0, 180)
